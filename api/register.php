073471e17175d1b30c2e5614ffe715e3a6397bf1
jorgemora <jorgeguitars@hotmail.com>
2024-10-18 08:31:28 -0500

    Creacion de la funcion register
    Se crea la funcion del api para creacion de usuarios

---
+++ api/register.php
@@ -0,0 +1,30 @@
+<?php
+//register
+include '../config/database.php'; // Asegúrate de que la ruta sea correcta
+
+header('Content-Type: application/json; charset=utf-8');
+// Crear una instancia de la clase Database
+$database = new Database();
+$conn = $database->getConnection(); // Obtener la conexión
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Verifica si los datos están presentes
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+        // Preparar la consulta
+        $stmt = $conn->prepare('INSERT INTO users (name, email, password) VALUES (?, ?, ?)');
+
+        if ($stmt) {
+            $stmt->execute([$name, $email, $password]);
+            echo json_encode(['message' => 'Usuario registrado con éxito'], JSON_UNESCAPED_UNICODE);
+        } else {
+            echo json_encode(['error' => 'No se pudo preparar la consulta'], JSON_UNESCAPED_UNICODE);
+        }
+    } else {
+        echo json_encode(['error' => 'Faltan parámetros en la solicitud'], JSON_UNESCAPED_UNICODE);
+    }
+}
+?>
